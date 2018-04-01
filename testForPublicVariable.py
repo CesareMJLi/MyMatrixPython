14bfd428e7ba78e7f1c85e37921079198aee03b1
Unknown <cesareli1995@gmail.com>
2018-04-01 10:26:30 +0200

    Done :)

---
+++ testForPublicVariable.py
@@ -0,0 +1,31 @@
+
+
+'''
+
+test for public variable
+
+'''
+
+
+# x1=1
+
+# y2=2
+
+# print x1
+
+# class cal:
+#     def __init__(self, v):
+#         self.val=v
+
+#     def changex(self):
+#         x1=10
+
+# calculation=cal(9)
+# calculation.changex()
+
+# print x1
+
+char_set = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5}
+from random import choice
+chars = choice(char_set.keys())
+print choice(chars)
